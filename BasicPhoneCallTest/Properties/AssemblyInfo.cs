6c98a80e47732fc8509c4bbb1fa0a32d3c2f137b
brooksy <david.brooks@live.ca>
2018-02-11 22:30:46 -0500

    Updated structure

--- BasicPhoneCallTest/Properties/AssemblyInfo.cs
+++ BasicPhoneCallTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleFiniteStateMachineTest")]
+[assembly: AssemblyTitle("BasicPhoneCallTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleFiniteStateMachineTest")]
+[assembly: AssemblyProduct("BasicPhoneCallTest")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("af348e3b-80cf-430c-b179-ccc594ce3037")]
+[assembly: Guid("1e692b95-2586-40d5-99f2-a529c030fab3")]
 
 // Version information for an assembly consists of the following four values:
 //
